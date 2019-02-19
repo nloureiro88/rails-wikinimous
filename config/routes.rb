329c71236a531efc93223fc0a7070879a001bbea
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-02-19 16:37:43 +0000

    All features created for Wiki, including initial CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index', as: :articles
+  post 'articles', to: 'articles#create'
+
+  get 'articles/new', to: 'articles#new', as: :new_article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  get 'articles/:id', to: 'articles#show', as: :article
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
+
+  root to: 'articles#index'
 end
